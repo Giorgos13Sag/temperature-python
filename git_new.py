e1967c73ba33dddeb1153dba686bfcaeefc7c9a2
Giorgos Panagiotopoulos <oldholborn2611@gmail.com>
2024-05-25 23:57:10 +0300

    explain temp

--- git_new.py
+++ git_new.py
@@ -1,5 +1,6 @@
+#Βιβλιοθηκη
 from tkinter import *
-
+#Συναρτηση
 def toCelsius():
     fahrenheit = float(temperature.get())
     celsius = round((fahrenheit - 32) * 5.0/9.0 ,2)
@@ -10,23 +11,28 @@ def toFahrreinheit():
     fahrenheit = round(9.0/5.0 *celsius + 32 ,2)
     result["text"] = f"Οι {celsius} βαθμοι κελσιου ειναι {fahrenheit} βαθμοι Φαραναιτ "
 
-
+#Δημηιουργια Παραθυρου window = root
 root = Tk()
 root.title("Γιωργος Dev")
 root.geometry("1280x720")
-
+#Δημιουργια Κειμενου
 label = Label(root, text="Μετατροπη σε μοναδες Celsius/Fahrenheit")
 label.pack()
 
+#Δημιουργια εισαγωγης θερμοκρασιας
+
 temperature=Entry(root)
 temperature.pack()
-
+#Αποτελεσμα συναρτησης
 result = Label(text="")
 result.pack()
+
+#Τα κουμπια για ενεργοποιηση προγραμματος
 
 btn1 = Button(root, text ="Μετατροπη σε βαλθμους Κελσιου", command=toCelsius)
 btn2 = Button(root, text="Μετατροπη σε βαθμους Φαραναιτ", command=toFahrreinheit )
 btn1.pack()
 btn2.pack()
 
+#Ο τροπος για να μενει το παραθυρο σταθερα ανοιχτο
 root.mainloop()
